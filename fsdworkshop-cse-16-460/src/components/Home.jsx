7dc6c0ecb7c67054c747964171f15ea0e3f67b36
Dhruv Talan <dhruvtalan22107@gmail.com>
2026-09-01 11:05:22 +0530

    Add workshop files

---
+++ fsdworkshop-cse-16-460/src/components/Home.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import images from "../assets/oogway.jpg";
+
+const Home = () => {
+  return (
+    <div>Home
+        <h2>Welcome to first component</h2>
+        <img
+        src={images}
+        alt="My Image"
+        width="400"
+      />
+    </div>
+  )
+}
+
+export default Home
